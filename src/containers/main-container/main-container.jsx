2405a733f3545d8d0a36b141ab34bb7e7aeb31d9
Viacheslav_Ignatov <Viacheslav_Ignatov@epam.com>
2019-02-18 23:10:43 +0300

    Add new team member creation feature

--- src/containers/main-container/main-container.jsx
+++ src/containers/main-container/main-container.jsx
@@ -1,26 +1,73 @@
 import React, { useState } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { Fab, TextField } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
 import { Flex } from '@rebass/grid'
 import uniqid from 'uniqid'
 
 import { Table, LineChart } from '../../components'
 
+const styles = theme => ({
+  fab: {
+    margin: theme.spacing.unit,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
+  textField: {
+    marginRight: theme.spacing.unit,
+  },
+  dense: {
+    marginTop: 16,
+  },
+})
+
 const fakeSprints = [
   {
     id: uniqid(),
     number: 1,
-    team: [{ id: uniqid(), name: 'Tom' }, { id: uniqid(), name: 'Jerry' }, { id: uniqid(), name: 'Spike' }],
+    team: [],
   },
 ]
-export const MainContainer = () => {
-  const [sprints, setSprints] = useState(fakeSprints)
+const MainContainerComponent = ({ classes }) => {
   const [currentSprint, setCurrentSprint] = useState(fakeSprints[0])
+  const [name, setName] = useState('')
 
   return (
     <Flex justifyContent="center">
       <Flex flexDirection="column" width={3 / 4}>
+        <Flex width={1 / 2} my="2rem" alignItems="center">
+          <TextField
+            id="outlined-name"
+            label="Team member name"
+            className={classes.textField}
+            value={name}
+            onChange={e => setName(e.target.value)}
+            margin="dense"
+            variant="outlined"
+          />
+
+          <Fab
+            size="small"
+            color="primary"
+            aria-label="Add"
+            className={classes.fab}
+            disabled={!name}
+            onClick={() => {
+              if (name) {
+                setCurrentSprint({ ...currentSprint, team: [...currentSprint.team, { id: uniqid(), name }] })
+                setName('')
+              }
+            }}>
+            <AddIcon />
+          </Fab>
+        </Flex>
+
         <Table sprint={currentSprint} setCurrentSprint={setCurrentSprint} />
         <LineChart currentSprint={currentSprint} />
       </Flex>
     </Flex>
   )
 }
+
+export const MainContainer = withStyles(styles)(MainContainerComponent)
